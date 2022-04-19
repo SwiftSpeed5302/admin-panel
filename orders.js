aea2b9c3be71bba8a1d4447147550eb9dd32e222
SwiftSpeed5302 <87231490+SwiftSpeed5302@users.noreply.github.com>
2022-04-19 14:22:06 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "83749",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Model O Drone",
+    productNumber: "25525",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Rudo 11 Pro Drone",
+    productNumber: "55636",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "83749",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "AK 47 Drone",
+    productNumber: "83749",
+    paymentStatus: "Paid",
+    shipping: "Pending",
+  },
+  {
+    productName: "GPS 2k Drone",
+    productNumber: "89589",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "White Pro Drone",
+    productNumber: "24564",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
